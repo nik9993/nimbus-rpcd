36535f3966f65b7918c7390e7eff0491dc7ba11f
Nick P <nik9993@gmail.com>
2016-05-25 10:10:50 -0500

    Changes to be committed: 	modified:   nimbus-rpcd.sh

--- nimbus-rpcd.sh
+++ nimbus-rpcd.sh
@@ -1,1 +1,26 @@
 #!/bin/sh
+
+# include the json sh library
+. /usr/lib/nimbus/lib.sh
+
+case "$1" in
+    list)
+            echo '{"func1": {"arg1":"value", "arg2":"value"}, "func2": {"arg1":"value"}, "func3":{}}'
+    ;;
+    call}
+            case "$2" in
+                func1)
+                    read input
+                    echo '{"hello":"world", "func":"tion1"}'
+                ;;
+                func2)
+                    read input
+                    echo '{"hello":"world", "func":"tion2"}'
+                ;;
+                func3)
+                    read input
+                    echo '{"func":"tion3"}'
+                ;;
+            esace
+    ;;
+esac
